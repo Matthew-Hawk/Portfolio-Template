b470cc4bb38f96a5750d9f2ae9f2518f69a81a7c
Ados4453 <matthawkins44@gmail.com>
2022-09-14 22:51:42 -0500

    did basic styling

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,13 +1,14 @@
-
+import './Home.scss'
 
 function Home() {
   return (
     <div className="home">
-        <div className="service">
-            <div className="service__div">
+            <div className="service">
                 <button className="service__button">Netflix</button>
             </div>
-        </div>
+            <section>
+              <img></img>
+            </section>
         <div className="button">
             <button className="button__btn">Pick That Flick</button>
         </div>
