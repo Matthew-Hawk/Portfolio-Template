6b38eef3ea9813d181478258fa9477931b92e079
Ados4453 <matthawkins44@gmail.com>
2022-09-14 19:57:14 -0500

    created global/mixin/utility style sheets and linked them to the header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -6,13 +6,13 @@ function Header() {
     return (
       <div className="header">
         <div className="logo">
-         <Link to='/' className="link">
+         <Link to='/' className="header__link">
           <p className="logo__1">FLICK</p>
           <p className="logo__2">FINDER</p>
          </Link>
         </div>
         <div className="contact">
-         <Link to='/contact' className="link">
+         <Link to='/contact' className="header__link">
           <p className="contact-text">CONTACT</p>
          </Link>
         </div>
