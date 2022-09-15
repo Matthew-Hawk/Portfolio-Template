4b2e764da4f7fe05487d3881813dc8c285596d5b
Ados4453 <matthawkins44@gmail.com>
2022-09-14 22:06:43 -0500

    did basic styling for the content page, also added a live gradient to the background of the app.js so it is present throught the app

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -3,11 +3,11 @@ import "./Contact.scss"
 function Contact() {
   return (
     <div className="main">
-        <div>
-            <a href="https://www.linkedin.com/in/matthew-hawk/">Linked-in</a>
+        <div className="link-div">
+            <a className="link-div__tag" href="https://www.linkedin.com/in/matthew-hawk/">Linked-in</a>
         </div>
-        <div>
-            <a href="https://github.com/Ados4453">Github</a>
+        <div className="link-div">
+            <a className="link-div__tag" href="https://github.com/Ados4453">Github</a>
         </div>
     </div>
   )
