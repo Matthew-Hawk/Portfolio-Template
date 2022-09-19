0b2f5318ea75c5f5d053e7ed33a03825b4801079
Ados4453 <matthawkins44@gmail.com>
2022-09-19 18:09:45 -0500

    fixed spacing of the buttons on the home page as well as font sizing on all breakpoints to better fit

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -5,11 +5,11 @@ function Home() {
 
 
 
-  
+
   return (
     <div className="home">
-            <div className="Genre">
-                <button className="service__button">Horror</button>
+            <div className="genre">
+                <button className="genre__button">Horror</button>
             </div>
             <section>
               <img></img>
