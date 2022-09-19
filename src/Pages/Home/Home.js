3a58d650c1666e528e05e0b6162fc62a222d21da
Ados4453 <matthawkins44@gmail.com>
2022-09-19 18:04:29 -0500

    swapped Netflix button to horror button, will work on fixing margins between breakpoints

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,15 @@
 import './Home.scss'
 
 function Home() {
+
+
+
+
+  
   return (
     <div className="home">
-            <div className="service">
-                <button className="service__button">Netflix</button>
+            <div className="Genre">
+                <button className="service__button">Horror</button>
             </div>
             <section>
               <img></img>
