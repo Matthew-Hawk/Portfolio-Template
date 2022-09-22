0df520b360ff2082f54b97943101130d90dacb56
Ados4453 <matthawkins44@gmail.com>
2022-09-21 19:27:33 -0500

    created files for Hot Fuzz movie and set up request to get required data for page

---
+++ src/Pages/Hot Fuzz/Hot-Fuzz.js
@@ -0,0 +1,44 @@
+import './Hot-Fuzz.scss'
+import Couch from '../../Assets/Photos/05-couch.png'
+import axios from 'axios'
+import {useState, useEffect} from 'react'
+
+function Shaun() {
+    const[title, setTitle] = useState()
+    const[poster, setPoster] = useState()
+    const[overview, setOverview] = useState()
+    const[tagline, setTagline] = useState()
+
+    const API = "http://localhost:8080/hotfuzz"
+
+
+    useEffect(() =>{
+        axios.get(API)
+        .then((response) => {
+            console.log(response.data)
+            setTitle(response.data.original_title)
+            setPoster(response.data.poster_path)
+            setOverview(response.data.overview)
+            setTagline(response.data.tagline)
+            console.log(response.data)
+        })
+    }, [])
+
+
+  return (
+    <div className="home">
+        <h1 className="home__title">
+            {title}
+        </h1>
+        <p className="home__tagline">{tagline}</p>
+        <div className="content">
+            <img className="content__pic" src={Couch} />
+            <h2 className="content__ovT">Overview:</h2>
+            <p className="content__info">{overview}</p>
+        </div>
+
+    </div>
+  )
+}
+
+export default Shaun
