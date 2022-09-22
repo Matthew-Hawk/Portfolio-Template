726fe0328019989eab11cc442711ba95ddddd4b4
unknown <matthawkins44@gmail.com>
2022-09-22 02:33:37 -0500

    corrected styling errors on about page, and added 600 weight Montserrat font

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -8,7 +8,10 @@ function About() {
             <img className='photo-div__pic' src={Photo} alt="Portrait of Edgar Wright"></img>
             <p className='photo-div__i'>(Edgar Wright)</p>
         </div>
-        <p className='about__info'>Edgar Howard Wright (born 18 April 1974) is an English director, screenwriter, producer, and actor. He is best known for his comedic Three Flavours Cornetto film trilogy consisting of Shaun of the Dead (2004), Hot Fuzz (2007), and The World's End (2013), made with recurrent collaborators Simon Pegg, Nira Park and Nick Frost. He also collaborated with them as the director of the television series Spaced.</p>
+        <div className='text-div'>
+            <h2 className='text-div__title'>About</h2>
+            <p className='text-div__info'>Edgar Howard Wright (born 18 April 1974) is an English director, screenwriter, producer, and actor. He is best known for his comedic Three Flavours Cornetto film trilogy consisting of Shaun of the Dead (2004), Hot Fuzz (2007), and The World's End (2013), made with recurrent collaborators Simon Pegg, Nira Park and Nick Frost. He also collaborated with them as the director of the television series Spaced.</p>
+        </div>
     </div>
   )
 }
