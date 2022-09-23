c446fc68c0fd13f0cf1812da6a992fe8fd826fcb
unknown <matthawkins44@gmail.com>
2022-09-23 00:58:48 -0500

    created slide for the home page with movie posters, will link page to corrosponding movie poster

---
+++ src/Assets/Movie-Posters/Posters.js
@@ -0,0 +1,5 @@
+import poster1 from './Hot Fuzz poster.jpg'
+import poster2 from './Shaun of the dead poster.jpg'
+import poster3 from './Worlds end poster.jpg'
+
+export default [poster1, poster2, poster3]
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,11 +1,40 @@
 import './Home.scss'
 import {Link} from 'react-router-dom'
+import {motion} from 'framer-motion'
+import {useRef, useEffect, useState} from 'react'
+import Posters from '../../Assets/Movie-Posters/Posters'
 
 
 function Home() {
+    const[width, setWidth] = useState()
+
+    const caroesel = useRef()
+
+    useEffect(() =>{
+        console.log(caroesel.current.scrollWidth, caroesel.current.offsetWidth)
+        setWidth(caroesel.current.scrollWidth - caroesel.current.offsetWidth)
+      }, [])
   return (
     <div className="home">
-      <iframe src="https://giphy.com/embed/H62QNi23QLLlF8Ly9i" width="280" height="280" frameBorder="" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/workingtitlefilms-simon-pegg-shaun-of-the-dead-nick-frost-H62QNi23QLLlF8Ly9i"></a></p>
+      {/* <iframe src="https://giphy.com/embed/H62QNi23QLLlF8Ly9i" width="280" height="280" frameBorder="" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/workingtitlefilms-simon-pegg-shaun-of-the-dead-nick-frost-H62QNi23QLLlF8Ly9i"></a></p> */}
+      <motion.div 
+                ref={caroesel} 
+                whileTap={{cursor: "grabbing"}}
+                className="carousel">
+                    <motion.div
+                    drag='x'
+                    dragConstraints={{right: 0, left: -width}}
+                    className="carousel__inner">
+                        {Posters.map(pic => {
+                            return(
+                                <motion.div className="carousel__poster">
+                                    <img src={pic} alt='photo taken from movie' />
+                                    <h1>title</h1>
+                                </motion.div>
+                            )
+                        })}
+                    </motion.div>
+            </motion.div>
       <div className='button-div'>
         <Link to='/about'>
         <button className='button-div__btn'>About</button>
