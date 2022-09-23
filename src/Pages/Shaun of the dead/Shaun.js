a4e7904d92cf9c568fc63bcb76c7275619f6acc4
unknown <matthawkins44@gmail.com>
2022-09-22 23:02:24 -0500

    made slider for shaun of the daed images

--- src/Pages/Shaun of the dead/Shaun.js
+++ src/Pages/Shaun of the dead/Shaun.js
@@ -1,25 +1,35 @@
 import "./Shaun.scss"
-import Couch from "../../Assets/Photos/05-couch.png"
 import axios from 'axios'
-import {useState, useEffect} from "react"
+import { useRef, useState, useEffect} from "react"
+import { motion } from 'framer-motion'
+import images from '../../Assets/Shaun/Shaun-Img'
+
 
 function Shaun() {
     const[title, setTitle] = useState()
     const[overview, setOverview] = useState()
     const[tagline, setTagline] = useState()
+    const[width, setWidth] = useState(0)
 
+    const carousel = useRef()
     const API = "http://localhost:8080/shaun"
 
+    
 
     useEffect(() =>{
         axios.get(API)
         .then((response) => {
-            console.log(response.data)
+            // console.log(response.data)
             setTitle(response.data.original_title)
             setOverview(response.data.overview)
             setTagline(response.data.tagline)
-            console.log(response.data)
+            // console.log(response.data)
         })
+    }, [])
+
+    useEffect(() =>{
+        // console.log(carousel.current.scrollWidth, carousel.current.offsetWidth)
+        setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth)
     }, [])
 
 
@@ -30,7 +40,25 @@ function Shaun() {
         </a>
         <p className="shaun__tagline">{tagline}</p>
         <div className="cont">
-            <img className="cont__pic" src={Couch} />
+            <motion.div 
+            ref={carousel} 
+            whileTap={{cursor: "grabbing"}}
+            className="carousel">
+                <motion.div
+                 drag='x'
+                  dragConstraints={{right: 0, left: -width}}
+                   className="carousel__inner">
+                    {images.map(pic => {
+                        return(
+                            <motion.div className="carousel__item">
+                                <a href="https://www.youtube.com/watch?v=f9XZqB6pI5E&ab_channel=thecoolidge"> 
+                                <img src={pic} alt='photo taken from movie' />
+                                </a>
+                            </motion.div>
+                        )
+                    })}
+                </motion.div>
+            </motion.div>
             <h2 className="cont__ovT">Overview:</h2>
             <p className="cont__info">{overview}</p>
         </div>
