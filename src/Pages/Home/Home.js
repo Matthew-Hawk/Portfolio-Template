673bb7d185d1c7fa21457558649c313de47f79e8
TheHawk4453 Hawk <matthawkins44@gmail.com>
2022-09-25 23:58:57 -0500

    styled footer across break points, movng to home carousel

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -21,54 +21,65 @@ function Home() {
         setWidth(car.current.scrollWidth - car.current.offsetWidth)
       }, [])
 
+    useEffect(() =>{
+        setWidth(car.current.scrollWidth - car.current.offsetWidth)
+      }, [window.innerWidth])
+
 
   return (
     <div className="home">
       {/* <iframe src="https://giphy.com/embed/H62QNi23QLLlF8Ly9i" width="280" height="280" frameBorder="" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/workingtitlefilms-simon-pegg-shaun-of-the-dead-nick-frost-H62QNi23QLLlF8Ly9i"></a></p> */}
       <motion.div 
-                ref={car} 
-                whileTap={{cursor: "grabbing"}}
-                className="carousel">
+              ref={car} 
+              whileTap={{cursor: "grabbing"}}
+              className="carousel">
                     <motion.div
                     drag='x'
                     dragConstraints={{right: 0, left: -width}}
                     className="carousel__inner">
+                      {/* Shaun Poster */}
                       <motion.div className='carousel__poster'>
-                        <Link to='/shaun'>
+                        <Link  to='/shaun'>
                           <img src={ShaunPoster} />
-                          <h2 className='arousel_title'>Shaun of the Dead</h2>
+                          <p className='arousel_title'>Shaun of the Dead</p>
                         </Link>
                         </motion.div>
+                      {/* Hot Fuzz Poster */}
                       <motion.div className='carousel__poster'>
                         <Link to='/hotfuzz'>
                           <img src={HotPoster} />
-                          <h2 className='carousel_title'>Hot Fuzz</h2>
+                          <p className='carousel_title'>Hot Fuzz</p>
                         </Link>
                         </motion.div>
+                      {/* The Worlds End Poster */}
                       <motion.div className='carousel__poster'>
                         <Link to='/worldsend'>
                           <img src={EndPoster} />
                           <h2 className='arousel_title'>The Worlds End</h2>
                         </Link>
                         </motion.div>
+                      {/* Baby Driver Poster */}
                       <motion.div className='carousel__poster'>
                         <Link to='/babydriver'>
                           <img src={BabyPoster} />
                           <h2 className='arousel_title'>Baby Driver</h2>
                         </Link>
                       </motion.div>
+                      {/* Scott Pilgrim Poster */}
                       <motion.div className='carousel__poster'>
                         <Link to='/scott'>
                           <img src={ScottPoster} />
                           <h2 className='arousel_title'>Scott Pilgrim</h2>
                         </Link>
                       </motion.div>
+                      {/* Last Night in Soho Poster */}
                       <motion.div className='carousel__poster'>
                         <Link to='/last'>
                           <img src={LastPoster} />
                           <h2 className='arousel_title'>Last Night in Soho</h2>
                         </Link>
                       </motion.div>
+
                     </motion.div>
             </motion.div>
       <div className='button-div'>
